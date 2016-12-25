7c9e83150eddda036faa8c5625276875c5fa53ff
ioana <ioasitaru@yahoo.com>
2016-12-25 19:36:59 +0200

    GpsPushService

--- Meetable/app/src/main/java/de/htwg_konstanz/moco/meetable/MainActivity.java
+++ Meetable/app/src/main/java/de/htwg_konstanz/moco/meetable/MainActivity.java
@@ -1,13 +1,57 @@
 package de.htwg_konstanz.moco.meetable;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(!runtimePermissions())
+            //send to server
+            pushToServer();
+
     }
+
+    private boolean runtimePermissions(){
+        if(Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String [] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},100 );
+        return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==100){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED && grantResults[1]==PackageManager.PERMISSION_GRANTED){
+                //send to server
+                pushToServer();
+            }
+            else
+                runtimePermissions();
+        }
+    }
+
+    private void pushToServer() {
+
+        Intent i= new Intent(getApplicationContext(),GPSPushService.class);
+        startService(i);
+
+
+    }
+
+
 }
