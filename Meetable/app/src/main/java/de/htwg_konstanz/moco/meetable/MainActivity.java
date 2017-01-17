d44d067818399dd6d08497420af13c682c756b33
ioana <ioasitaru@yahoo.com>
2017-01-17 12:49:54 +0100

    push service

--- Meetable/app/src/main/java/de/htwg_konstanz/moco/meetable/MainActivity.java
+++ Meetable/app/src/main/java/de/htwg_konstanz/moco/meetable/MainActivity.java
@@ -1,63 +1,56 @@
 package de.htwg_konstanz.moco.meetable;
-
 import android.Manifest;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.provider.Settings;
-import android.support.v4.app.ActivityCompat;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
-
-    // Broadcast receiver for receiving status updates from the IntentService
-    private class ResponseReceiver extends BroadcastReceiver
-    {
-        // Called when the BroadcastReceiver gets an Intent it's registered to receive
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (intent.getAction().equals(GpsPullService.STATUS_REPORT_ACTION)){
-                System.out.println("Latitude: " + intent.getStringExtra(GpsPullService.STATUS_REPORT_LATITUDE)+
-                        "\nLongitude: " +intent.getStringExtra(GpsPullService.STATUS_REPORT_LONGITUDE));
-            }
-        }
-    }
-
-    private ResponseReceiver receiver;
-
+    private Button button_friendscoordonates;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        System.out.println("Starting up service");
-        IntentFilter filter = new IntentFilter(GpsPullService.STATUS_REPORT_ACTION);
-        filter.addCategory(Intent.CATEGORY_DEFAULT);
-        receiver = new ResponseReceiver();
-        registerReceiver(receiver, filter);
-
-        ActivityCompat.requestPermissions(
-                this,
-                new String[]{Manifest.permission.INTERNET},
-                1);
-
-        ActivityCompat.requestPermissions(
-                        this,
-                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
-                        1);
-
-
-
-        try{
-            //GpsPullService.startActionGetLocation(getApplicationContext());
-            GpsPullService.startActionGetLocation(this.getBaseContext());
-        } catch (Exception ex){
-            System.out.println(ex.getMessage());
-            System.out.println(ex.getStackTrace());
+        button_friendscoordonates=(Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView2);
+        if(!runtimePermissions()) {
+            enableButtons();
+            Intent i= new Intent(getApplicationContext(),GPSPushService.class);
+            startService(i);
         }
-
-
+    }
+    private void enableButtons() {
+        button_friendscoordonates.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                // i need the PullService for enabling the button
+                Intent i2= new Intent(getApplicationContext(),GPSPullService.class);
+                startService(i2);
+            }
+        });
+    }
+    private boolean runtimePermissions(){
+        if(Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String [] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},100 );
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==100){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED && grantResults[1]==PackageManager.PERMISSION_GRANTED){
+                enableButtons();
+            }
+            else
+                runtimePermissions();
+        }
     }
 }
