81a360f4a840c5c5d9eed2b6983cff7d62719c34
bencikpeter <bencik.peter.ml@gmail.com>
2017-01-25 12:54:45 +0100

    WelcomeActivity first + button opens maps

---
+++ Meetable/app/src/main/java/de/htwg_konstanz/moco/meetable/WelcomeActivity.java
@@ -0,0 +1,24 @@
+package de.htwg_konstanz.moco.meetable;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainpage);
+    }
+
+    public void goToMaps(View view) {
+        Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+        startActivity(intent);
+    }
+
+}
